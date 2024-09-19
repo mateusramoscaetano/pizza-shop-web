81a05d0e185f9d6bee3c3a82f77ace46107cc67c
Mateus Caetano <mateus-ramoshd@hotmail.com>
2024-09-19 14:50:51 -0300

    Add pagination on orders table, missing implementations

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,16 +1,14 @@
 import { Helmet } from "react-helmet-async";
-import { Input } from "../../../components/ui/input";
 import {
   Table,
   TableBody,
-  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "../../../components/ui/table";
-import { Button } from "../../../components/ui/button";
-import { ArrowRight, Search, X } from "lucide-react";
 import { OrderTableRow } from "./order-table-row";
+import { OrderTableFilters } from "./order-table-filters";
+import { Pagination } from "../../../components/pagination";
 
 interface IOrdersProps {}
 
@@ -20,31 +18,30 @@ export function Orders({}: IOrdersProps) {
       <Helmet title="Pedidos" />
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-      </div>
-      <div className="space-y-2.5">
-        <form action="" className="flex items-center  gap-2">
-          <span className="text-sm font-semibold">Filtros:</span>
-          <Input placeholder="Nome do cliente" className="h-8 w-[320px]" />
-        </form>
-        <div className="border rounded-md">
-          <Table>
-            <TableHeader></TableHeader>
-            <TableRow>
-              <TableHead className="w-[64px]"></TableHead>
-              <TableHead className="w-[140px]">Id</TableHead>
-              <TableHead className="w-[180px]">Realizado há</TableHead>
-              <TableHead className="w-[140px]">Status</TableHead>
-              <TableHead>Cliente</TableHead>
-              <TableHead className="w-[140px]">Total do pedido</TableHead>
-              <TableHead className="w-[164px]"></TableHead>
-              <TableHead className="w-[132px]"></TableHead>
-            </TableRow>
-            <TableBody>
-              {Array.from({ length: 10 }).map((_, i) => {
-                return <OrderTableRow key={i.toString()} />;
-              })}
-            </TableBody>
-          </Table>
+
+        <div className="space-y-2.5">
+          <OrderTableFilters />
+          <div className="border rounded-md">
+            <Table>
+              <TableHeader></TableHeader>
+              <TableRow>
+                <TableHead className="w-[64px]"></TableHead>
+                <TableHead className="w-[140px]">Id</TableHead>
+                <TableHead className="w-[180px]">Realizado há</TableHead>
+                <TableHead className="w-[140px]">Status</TableHead>
+                <TableHead>Cliente</TableHead>
+                <TableHead className="w-[140px]">Total do pedido</TableHead>
+                <TableHead className="w-[164px]"></TableHead>
+                <TableHead className="w-[132px]"></TableHead>
+              </TableRow>
+              <TableBody>
+                {Array.from({ length: 10 }).map((_, i) => {
+                  return <OrderTableRow key={i.toString()} />;
+                })}
+              </TableBody>
+            </Table>
+          </div>
+          <Pagination pageIndex={0} totalCount={105} perPage={10} />
         </div>
       </div>
     </>
