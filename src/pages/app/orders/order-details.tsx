40b7d0926c2822a58a0cd8cb5be40986c4746071
Mateus Caetano <mateus-ramoshd@hotmail.com>
2024-09-21 10:11:40 -0300

    Add skeleton to order details dialog for await data fetch

--- src/pages/app/orders/order-details.tsx
+++ src/pages/app/orders/order-details.tsx
@@ -19,6 +19,7 @@ import { OrderStatus } from "../../../components/order-status";
 import dayjs from "dayjs";
 import "dayjs/locale/pt-br";
 import relativeTime from "dayjs/plugin/relativeTime";
+import { OrderDetailsSkeleton } from "./order-details-skeleton";
 
 dayjs.locale("pt-br");
 dayjs.extend(relativeTime);
@@ -35,10 +36,6 @@ export function OrderDetails({ orderId, open }: IOrderDetailsProps) {
     enabled: open,
   });
 
-  if (!order) {
-    return null;
-  }
-
   return (
     <>
       <DialogContent>
@@ -46,96 +43,104 @@ export function OrderDetails({ orderId, open }: IOrderDetailsProps) {
           <DialogTitle>Pedido: {orderId}</DialogTitle>
           <DialogDescription>Detalhes do Pedido</DialogDescription>
         </DialogHeader>
-        <div className="space-y-6">
-          <Table>
-            <TableBody>
-              <TableRow>
-                <TableCell className="text-muted-foreground">Status</TableCell>
-                <TableCell className="flex justify-end">
-                  <OrderStatus status={order.status} />
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell className="text-muted-foreground">Cliente</TableCell>
-                <TableCell className="flex justify-end">
-                  {order.customer.name}
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell className="text-muted-foreground">
-                  Telefone
-                </TableCell>
-                <TableCell className="flex justify-end">
-                  {order.customer.phone}
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell className="text-muted-foreground">Email</TableCell>
-                <TableCell className="flex justify-end">
-                  {order.customer.email}
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell className="text-muted-foreground">
-                  Realizado há
-                </TableCell>
-                <TableCell className="flex justify-end">
-                  {dayjs(order.createdAt).fromNow()}
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
+        {order ? (
+          <div className="space-y-6">
+            <Table>
+              <TableBody>
+                <TableRow>
+                  <TableCell className="text-muted-foreground">
+                    Status
+                  </TableCell>
+                  <TableCell className="flex justify-end">
+                    <OrderStatus status={order.status} />
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="text-muted-foreground">
+                    Cliente
+                  </TableCell>
+                  <TableCell className="flex justify-end">
+                    {order.customer.name}
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="text-muted-foreground">
+                    Telefone
+                  </TableCell>
+                  <TableCell className="flex justify-end">
+                    {order.customer.phone}
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="text-muted-foreground">Email</TableCell>
+                  <TableCell className="flex justify-end">
+                    {order.customer.email}
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="text-muted-foreground">
+                    Realizado há
+                  </TableCell>
+                  <TableCell className="flex justify-end">
+                    {dayjs(order.createdAt).fromNow()}
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
 
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead>Produto</TableHead>
-                <TableHead className="text-right">Qtd.</TableHead>
-                <TableHead className="text-right">Preço</TableHead>
-                <TableHead className="text-right">Subtotal</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {order.orderItems.map((item) => {
-                return (
-                  <TableRow key={item.id}>
-                    <TableCell>{item.product.name}</TableCell>
-                    <TableCell className="text-right">
-                      {item.quantity}
-                    </TableCell>
-                    <TableCell className="text-right">
-                      {(item.priceInCents / 100).toLocaleString("pt-BR", {
-                        style: "currency",
-                        currency: "BRL",
-                      })}
-                    </TableCell>
-                    <TableCell className="text-right">
-                      {" "}
-                      {(
-                        (item.priceInCents * item.quantity) /
-                        100
-                      ).toLocaleString("pt-BR", {
-                        style: "currency",
-                        currency: "BRL",
-                      })}
-                    </TableCell>
-                  </TableRow>
-                );
-              })}
-            </TableBody>
-            <TableFooter>
-              <TableRow>
-                <TableCell colSpan={3}>Total do pedido</TableCell>
-                <TableCell className="text-right font-medium">
-                  {(order.totalInCents / 100).toLocaleString("pt-BR", {
-                    style: "currency",
-                    currency: "BRL",
-                  })}
-                </TableCell>
-              </TableRow>
-            </TableFooter>
-          </Table>
-        </div>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Produto</TableHead>
+                  <TableHead className="text-right">Qtd.</TableHead>
+                  <TableHead className="text-right">Preço</TableHead>
+                  <TableHead className="text-right">Subtotal</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {order.orderItems.map((item) => {
+                  return (
+                    <TableRow key={item.id}>
+                      <TableCell>{item.product.name}</TableCell>
+                      <TableCell className="text-right">
+                        {item.quantity}
+                      </TableCell>
+                      <TableCell className="text-right">
+                        {(item.priceInCents / 100).toLocaleString("pt-BR", {
+                          style: "currency",
+                          currency: "BRL",
+                        })}
+                      </TableCell>
+                      <TableCell className="text-right">
+                        {" "}
+                        {(
+                          (item.priceInCents * item.quantity) /
+                          100
+                        ).toLocaleString("pt-BR", {
+                          style: "currency",
+                          currency: "BRL",
+                        })}
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+              <TableFooter>
+                <TableRow>
+                  <TableCell colSpan={3}>Total do pedido</TableCell>
+                  <TableCell className="text-right font-medium">
+                    {(order.totalInCents / 100).toLocaleString("pt-BR", {
+                      style: "currency",
+                      currency: "BRL",
+                    })}
+                  </TableCell>
+                </TableRow>
+              </TableFooter>
+            </Table>
+          </div>
+        ) : (
+          <OrderDetailsSkeleton />
+        )}
       </DialogContent>
     </>
   );
