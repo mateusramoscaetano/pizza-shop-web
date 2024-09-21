048f0fdf92cd88044cc18a52d49c0219a76d51a5
Mateus Caetano <mateus-ramoshd@hotmail.com>
2024-09-21 09:46:36 -0300

    Add metric card skeleton to await data fetch from metric cards

---
+++ src/pages/app/dashboard/cards/metric-card-skeleton.tsx
@@ -0,0 +1,12 @@
+import { Skeleton } from "../../../../components/ui/skeleton";
+
+interface IMetricCardSkeletonProps {}
+
+export function MetricCardSkeleton({}: IMetricCardSkeletonProps) {
+  return (
+    <>
+      <Skeleton className=" mt-1 h-7 w-36" />
+      <Skeleton className="h-4 w-52" />
+    </>
+  );
+}
